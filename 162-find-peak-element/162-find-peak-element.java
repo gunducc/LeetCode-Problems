5c69d2054d6b003011f6271403c6f3f304496408
Cengiz Gündüç <gunduc@gmail.com>
2022-08-24 11:34:04 +0300

    Time: 0 ms (100.00%), Space: 43.1 MB (30.45%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right){
+            int pivot = left + (right - left)/2;
+            if (nums[pivot] > nums[pivot+1])
+                right = pivot;
+            else
+                left = pivot + 1;
+        }
+        return left;
+    }
+}
