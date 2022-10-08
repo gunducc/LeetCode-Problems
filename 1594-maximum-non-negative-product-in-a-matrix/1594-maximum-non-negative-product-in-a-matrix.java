9b7a564b6b45f536bd3e238657e2b3f52f868b3c
Cengiz Gündüç <gunduc@gmail.com>
2022-10-08 21:22:10 +0300

    Time: 655 ms (9.74%), Space: 42.8 MB (61.69%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.java
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.java
@@ -1,24 +1,34 @@
 class Solution {
-    int mod = 1000000007;
+    
     long product = -1;
+    int mod = 1000000007;
     
     public int maxProductPath(int[][] grid) {
-        dfs(grid,0,0,grid[0][0]);    
-        return (int)(product%mod);
+         
+         dfs(grid, 0, 0, grid[0][0]);
+         return (int)(product%mod);
     }
     
-    public void dfs(int[][] grid, int i, int j, long curr){
-        if(i==grid.length-1 && j==grid[0].length-1){
-            product=Math.max(product,curr);
+    public void dfs(int[][] grid, int x, int y, long curr){
+ 
+        if (grid[x][y]==0){
+            product = Math.max(product,0);
             return;
         }
-        if(grid[i][j]==0){
-            product=Math.max(product,0);
-            return;
+            
+
+        if (x==grid.length-1 && y==grid[0].length-1){
+            product = Math.max(product,curr);
+            return;   
         }
-        if(i+1<grid.length)
-            dfs(grid,i+1,j,curr*grid[i+1][j]);
-        if(j+1<grid[0].length)
-            dfs(grid,i,j+1,curr*grid[i][j+1]);
+        
+        if (x+1<grid.length)
+            dfs(grid, x+1, y, curr*grid[x+1][y]);
+        
+        if (y+1<grid[0].length){
+            dfs(grid, x, y+1, curr*grid[x][y+1]);
+        }
+        
     }
+    
 }
